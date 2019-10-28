6c900307d1bd72a7be8eda4df8656ebebaea478b
Alexey-Bartashevich <melonyk5000@gmail.com>
2019-10-28 20:52:26 +0300

    Added files

---
+++ precache-manifest.e42cf46d6b55fe336254f1d96da3b708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58dc7afbae879ef0c3656af1e2fff2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2036d227970c2ddca663",
+    "url": "/static/css/main.934594d8.chunk.css"
+  },
+  {
+    "revision": "95111c547cc4e5b15c7e",
+    "url": "/static/js/2.d453b87a.chunk.js"
+  },
+  {
+    "revision": "2036d227970c2ddca663",
+    "url": "/static/js/main.90e5bdfd.chunk.js"
+  },
+  {
+    "revision": "d37e5e5e9932e30ff6cf",
+    "url": "/static/js/runtime-main.f6757b95.js"
+  }
+]);
